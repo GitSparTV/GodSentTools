303df333869d7c8b1e7175307bc74444d52fbbde
GitSparTV <5685050+GitSparTV@users.noreply.github.com>
2020-06-20 17:49:15 +0300

    Adaptive Crosshair | Hide if HUD is hidden

--- godsenttools_adaptive_crosshair/lua/autorun/client/godsent_adaptive_crosshair.lua
+++ godsenttools_adaptive_crosshair/lua/autorun/client/godsent_adaptive_crosshair.lua
@@ -1,23 +1,27 @@
-hook.Add("PostDrawHUD","GodSentToolsAdaptiveCrosshair",function()
-	local CW,CH = ScrW() * 0.5, ScrH() * 0.5
 
-	render.OverrideBlend(true, 3, 1, 1)
-	surface.SetDrawColor(255,255,255)
-	-- surface.SetDrawColor(0,0,0)
-	-- surface.DrawOutlinedRect(CW-16,CH-1,6,3)
-	-- surface.DrawOutlinedRect(CW-2,CH-13,3,6)
-	-- surface.DrawOutlinedRect(CW+9,CH-1,6,3)
-	-- surface.DrawOutlinedRect(CW-2,CH+8,3,6)
-	-- surface.DrawOutlinedRect(CW-1,CH-13,3,6)
-	surface.DrawLine(CW-12,CH,CW-6,CH)
-	surface.DrawLine(CW+9,CH,CW+3,CH)
+do
+	local surfaceDrawLine, ScrW, ScrH, renderOverrideBlend, surfaceSetDrawColor = surface.DrawLine, ScrW, ScrH, render.OverrideBlend, surface.SetDrawColor
 
-	surface.DrawLine(CW-1,CH-9,CW-1,CH-3)
-	surface.DrawLine(CW-1,CH+8,CW-1,CH+2)
-	surface.DrawLine(CW-2,CH-1,CW-1,CH)
-	render.OverrideBlend(false)
-end)
+	hook.Add("PostDrawHUD","GodSentToolsAdaptiveCrosshair",function()
+		if not hook.Run("HUDShouldDraw","CHudGMod") then return end
+		renderOverrideBlend(true, 3, 1, 1)
+		surfaceSetDrawColor(255, 255, 255)
 
-hook.Add("HUDShouldDraw","GodSentToolsAdaptiveCrosshair",function(n)
-	if n == "CHudCrosshair" then return false end
+		do
+			local CW, CH = ScrW() * 0.5, ScrH() * 0.5
+			surfaceDrawLine(CW - 12, CH, CW - 6, CH)
+			surfaceDrawLine(CW + 9, CH, CW + 3, CH)
+			surfaceDrawLine(CW - 1, CH - 9, CW - 1, CH - 3)
+			surfaceDrawLine(CW - 1, CH + 8, CW - 1, CH + 2)
+			surfaceDrawLine(CW - 2, CH - 1, CW - 1, CH)
+		end
+
+		renderOverrideBlend(false)
+	end)
+end
+
+hook.Add("HUDShouldDraw", "GodSentToolsAdaptiveCrosshair", function(n)
+	if n == "CHudCrosshair" then
+		return false
+	end
 end)
