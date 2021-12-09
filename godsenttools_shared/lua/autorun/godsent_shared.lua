2f59aac0ee8c5c60fb4583b815fcc146375e5fba
GitSparTV <developspartv@gmail.com>
2021-12-09 19:29:27 +0300

    Remove shared folder

--- godsenttools_shared/lua/autorun/godsent_shared.lua
+++
@@ -1,8 +0,0 @@
-if CLIENT then
-	language.Add("godsenttools.name", "GodSent Tools")
-	language.Add("godsenttools.enable","Enable")
-
-	hook.Add("AddToolMenuCategories", "GodSentToolsCategory", function()
-		spawnmenu.AddToolCategory("Utilities", "GodSent Tools", "#godsenttools.name")
-	end)
-end
