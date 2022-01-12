ed23e856c782c22e5c966a4bc20c8c4a5917b6dc
GitSparTV <developspartv@gmail.com>
2022-01-13 02:22:01 +0300

    Release of GPU Saver

--- godsenttools_gpu_saver/lua/autorun/client/godsent_gpu_saver.lua
+++ godsenttools_gpu_saver/lua/autorun/client/godsent_gpu_saver.lua
@@ -1,20 +1,9 @@
-local convar = CreateClientConVar("godsent_gpu_saver", "1", true, false, "Enables GPU Saving when the game is minimized", 0, 1)
-local convar_load = CreateClientConVar("godsent_load_notification", "1", true, false, "Enables notification when the game is loaded.", 0, 1)
+local convar = CreateClientConVar("godsenttools_gpu_saver", "1", true, false, "Enables GPU Saver, this is required to work in automatic and manual mode", 0, 1)
 local manual = false
-local KeyboardController
-
-do
-	language.Add("godsenttools.gpusaver.name", "GPU Saver")
-	language.Add("godsenttools.gpusaver.description", "\"GPU Saver\" disables entire rendering when the game window is not focused or minimized. This helps to reduce GPU load and maybe save you from VRAM crash.")
-	language.Add("godsenttools.gpusaver.osxwarning", "Warning! MacOS detected, this addon is not working with this operation system.\nThe addon will be automatically disabled.\nYou can still use this addon with \"Enable manually\" button.")
-	language.Add("godsenttools.gpusaver.enable.help", "Enables GPU Saver, this is required to work in automatic and manual mode.")
-	language.Add("godsenttools.gpusaver.enable.manual", "Enable manually")
-	language.Add("godsenttools.gpusaver.loadnotification.enable", "Enable \"Load Notification\"")
-	language.Add("godsenttools.gpusaver.loadnotification.enable.help", "\"Load Notification\" will inform you when the game is loaded by blinking the icon on the taskbar (Windows only) and playing a sound. Works only in windowed mode and when unfocused.")
-end
+local KeyboardController = NULL
 
 hook.Add("PopulateToolMenu", "GodSentToolsGPUSaver", function()
-	if IsValid(KeyboardController) then
+	if KeyboardController:IsValid() then
 		KeyboardController:Remove()
 	end
 
@@ -27,7 +16,7 @@ hook.Add("PopulateToolMenu", "GodSentToolsGPUSaver", function()
 		KeyboardController:SetMouseInputEnabled(false)
 	end
 
-	spawnmenu.AddToolMenuOption("Utilities", "GodSent Tools", "GodSent_GPU_Saver", "#godsenttools.gpusaver.name", "", "", function(form)
+	spawnmenu.AddToolMenuOption("Utilities", "GodSent Tools", "GodSentTools_GPU_Saver", "#godsenttools.gpusaver.name", "", "", function(form)
 		form:SetName("#godsenttools.gpusaver.name")
 		form:Help("#godsenttools.gpusaver.description")
 
@@ -36,7 +25,7 @@ hook.Add("PopulateToolMenu", "GodSentToolsGPUSaver", function()
 			convar:SetBool(false)
 		end
 
-		form:CheckBox("#godsenttools.enable", "godsent_gpu_saver")
+		form:CheckBox("#godsenttools.enable", "godsenttools_gpu_saver")
 		form:ControlHelp("#godsenttools.gpusaver.enable.help")
 
 		form:Button("#godsenttools.gpusaver.enable.manual").DoClick = function()
@@ -54,9 +43,6 @@ hook.Add("PopulateToolMenu", "GodSentToolsGPUSaver", function()
 				end
 			end)
 		end
-
-		form:CheckBox("#godsenttools.gpusaver.loadnotification.enable", "godsent_load_notification")
-		form:ControlHelp("#godsenttools.gpusaver.loadnotification.enable.help")
 	end)
 end)
 
@@ -68,7 +54,7 @@ do
 	local Line2h2, Line2h02, Line2h15 = Line2h * 2, Line2h * 0.5, Line2h * 1.5
 	local Line3len = surface.GetTextSize("To disable this entirely go to:") * 0.5
 	local Line4len = surface.GetTextSize("Q > Utilities > GodSent Tools > GPU Saver.") * 0.5
-	local surfaceSetTextColor, systemHasFocus, camStart, surfaceSetTextPos, surfaceSetFont, camEnd2D, surfaceDrawText, IsValid = surface.SetTextColor, system.HasFocus, cam.Start, surface.SetTextPos, surface.SetFont, cam.End2D, surface.DrawText, IsValid
+	local surfaceSetTextColor, systemHasFocus, camStart, surfaceSetTextPos, surfaceSetFont, camEnd2D, surfaceDrawText = surface.SetTextColor, system.HasFocus, cam.Start, surface.SetTextPos, surface.SetFont, cam.End2D, surface.DrawText
 	local surfaceSetDrawColor, surfaceDrawRect = surface.SetDrawColor, surface.DrawRect
 	local ScrW, ScrH = ScrW, ScrH
 	local state = false
@@ -80,7 +66,7 @@ do
 	local function GPUSaver()
 		if not systemHasFocus() or manual then
 			if not state then
-				if IsValid(KeyboardController) then
+				if KeyboardController:IsValid() then
 					KeyboardController:MakePopup()
 				end
 
@@ -107,7 +93,7 @@ do
 		elseif state then
 			state = false
 
-			if IsValid(KeyboardController) then
+			if KeyboardController:IsValid() then
 				KeyboardController:SetKeyboardInputEnabled(false)
 				KeyboardController:SetMouseInputEnabled(false)
 			end
@@ -118,19 +104,11 @@ do
 		hook.Add("PreRender", "GodSentToolsGPUSaver", GPUSaver)
 	end
 
-	cvars.AddChangeCallback("godsent_gpu_saver", function(_, _, newValue)
+	cvars.AddChangeCallback("godsenttools_gpu_saver", function(_, _, newValue)
 		if newValue == "1" then
 			hook.Add("PreRender", "GodSentToolsGPUSaver", GPUSaver)
 		else
 			hook.Remove("PreRender", "GodSentToolsGPUSaver")
 		end
 	end, "GodSentToolsGPUSaver")
-end
-
-if convar_load:GetBool() then
-	hook.Add("Think", "GodSentToolsGPUSaver", function()
-		system.FlashWindow()
-		surface.PlaySound("hl1/fvox/bell.wav")
-		hook.Remove("Think", "GodSentToolsGPUSaver")
-	end)
 end
